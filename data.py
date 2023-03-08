777fcef2ced4aa4262faa8c28a1116e004a3aaac
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-08 13:44:09 +0900

    시총 400위

--- data.py
+++ data.py
@@ -3,4 +3,8 @@ import streamlit as st
 
 @st.cache
 def load_etf_list():
-    return fdr.StockListing("ETF/KR")
+    return fdr.StockListing("ETF/KR")[:400]
+
+@st.cache
+def load_history(ticker):
+    return fdr.DataReader(ticker)
