578eb5c31f119c0ef635921ef410e97d6bb4b74e
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-08 13:52:09 +0900

    values

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 from data import *
 
 etf_list = load_etf_list()
-st.write(etf_list)
+# st.write(etf_list)
 
-df = load_history(etf_list.iloc[0, 0])
-pv = get_pv(df)
-st.write(pv)
+st.write(etf_list.Symbol.values())
+
+# df = load_history(etf_list.iloc[0, 0])
+# pv = get_pv(df)
+# st.write(pv)
