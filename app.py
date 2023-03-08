571787cc10c8132992a3dd81c1caab994c3585f9
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-08 13:53:17 +0900

    map2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from data import *
 etf_list = load_etf_list()
 # st.write(etf_list)
 
-histories = map(etf_list.Symbol, load_history)
+histories = map(load_history, etf_list.Symbol)
 st.write(histories)
 
 # df = load_history(etf_list.iloc[0, 0])
